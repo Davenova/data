198d63091de750ad403dd5e437c3662c75d59ad8
Unicorn X <dudebrothers2@gmail.com>
2024-10-08 01:48:09 +0530

    Update buttons.tsx

--- app/buttons.tsx
+++ app/buttons.tsx
@@ -3,10 +3,14 @@
 import React from 'react';
 
 export default function Buttons() {
+    const openYouTube = () => {
+        window.open('https://www.youtube.com', '_blank');
+    };
+
     return (
         <div>
             <h1>Basic Buttons</h1>
-            <button>Button 1</button>
+            <button onClick={openYouTube}>Button 1</button>
             <button>Button 2</button>
             <button>Button 3</button>
             <button>Button 4</button>
