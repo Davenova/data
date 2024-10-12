382ca471ad1984d751cad3e7bc286dbcf6bae595
Unicorn X <dudebrothers2@gmail.com>
2024-10-12 20:57:37 +0530

    Create joined-users.js

---
+++ app/api/joined-users.js
@@ -0,0 +1,19 @@
+// api/joined-users.js
+
+import { connectToDatabase } from '../../lib/mongodb';
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    try {
+      const { db } = await connectToDatabase();
+      const users = await db.collection('joinedUsers').find({}).toArray();
+
+      return res.status(200).json({ success: true, users });
+    } catch (error) {
+      return res.status(500).json({ error: 'Failed to fetch users' });
+    }
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
